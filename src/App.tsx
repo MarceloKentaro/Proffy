fd298d67c77c52f656420a8da32275f8d16fcbb1
MarceloKentaro <marcelo.funagoshi@gmail.com>
2020-09-20 16:02:24 -0300

    NLW - Proffy

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+//isso aqui é um componete e deve estar sempre com a primeira letra maiuscula
+import React from 'react';
+
+import Routes from './routes';
+
+import './assets/styles/global.css';
+
+
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
